54fd7fd2350a3776cabb624df00ba10987f7cb8a
Subhan Khan <subhank1997@gmail.com>
2023-08-09 17:05:29 -0400

    custom style for bottom right corner of container

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div class="bg-gray-100 flex justify-center items-center h-screen ">
 
-      <div id='container'  class="w-7/12 h-4/6 bg-white p-6 rounded-3xl">
+      <div id='container'  class="w-7/12 h-4/6 bg-white p-6 rounded-3xl rounded-br-brb">
 
         <div id='form' class="flex justify-start space-x-32 border-b-2 border-gray-200">
           <h4 class="">Day</h4>
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        'brb': '13rem'
+      }
+    },
   },
   plugins: [],
 }

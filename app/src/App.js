2ee0ea2bf716830ee40ea3419d2c4bf460d9943c
Subhan Khan <subhank1997@gmail.com>
2023-08-09 16:54:59 -0400

    added tailwind for styling and created basic layout

--- app/src/App.js
+++ app/src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="bg-gray-100 flex justify-center items-center h-screen ">
+
+      <div id='container'  class="w-7/12 h-4/6 bg-white p-6 rounded-3xl">
+
+        <div id='form' class="flex justify-start space-x-32 border-b-2 border-gray-200">
+          <h4 class="">Day</h4>
+          <h4 class="">Month</h4>
+          <h4 class="">Year</h4>
+        </div>
+
+        <div id='display' class="">
+          <h1>YEARS</h1>
+          <h1>MONTHS</h1>
+          <h1>DAYS</h1>
+        </div>
+
+      </div>
     </div>
   );
 }
